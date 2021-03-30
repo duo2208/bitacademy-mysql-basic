56a04920c8a0119482982d8c8fbcecd4bea07271
jimin lee <duo2208@likelion.org>
2021-03-30 11:53:09 +0900

    basic-sql.sql

--- sql-practice/basic-sql.sql
+++ sql-practice/basic-sql.sql
@@ -10,3 +10,13 @@ death date
 
 -- table scheme 확인
 desc pets;
+
+-- insert
+insert into pets values('키키', '교님', '요크셔테리어', '여', '2021-01-01', null);
+insert into pets(name, owner, species, gender, birth) values('폰펫', '이지민', '허스키', '여', '2021-03-30');
+insert into pets values('별이', '정서영', '말티즈', '남', '2018-01-01', null);
+
+-- select
+select * from pets;
+select name, birth from pets order by birth desc;
+select count(*) from pets;
